d78bc44a67f501f3f63de94050a1a3959f9eb57a
Saif <droconos99@gmail.com>
2025-12-12 23:54:15 +0600

    Add: Rest of the service setup

--- Order-Service/Program.cs
+++ Order-Service/Program.cs
@@ -1,9 +1,33 @@
+using Microsoft.AspNetCore.StaticFiles.Infrastructure;
+using RabbitMQ.Client;
+using StackExchange.Redis;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(ConnectionMultiplexer
+    .Connect("localhost:6379"));
+
+builder.Services.AddSingleton<IModel>(sp =>
+{
+    var connection = sp.GetRequiredService<IConnection>();
+    var channel = connection.CreateModel();
+
+    channel.ExchangeDeclare("order-events", ExchangeType.Topic, durable: true);
+    channel.QueueDeclare("inventory-queue", durable: true, exclusive: false);
+    channel.QueueDeclare("payment-queue", durable: true, exclusive: false);
+    channel.QueueDeclare("notification-queue", durable: true, exclusive: false);
+
+    channel.QueueBind("inventory-queue", "order-events", "order.created");
+    channel.QueueBind("payment-queue", "order-events", "inventory.reserved");
+    channel.QueueBind("notification-queue", "order-events", "payment.processed");
+
+    return channel;
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
